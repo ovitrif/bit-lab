61ff3bc84010eeadbd5c5991cfd84d3a7467060a
Ovi Trif <octrif@gmail.com>
2023-12-02 15:53:56 +0100

    Cleanup MempoolApi

--- app/src/main/java/tech/masivo/bitlab/data/sources/MempoolApi.kt
+++ app/src/main/java/tech/masivo/bitlab/data/sources/MempoolApi.kt
@@ -5,9 +5,6 @@ import retrofit2.http.Path
 import tech.masivo.bitlab.data.model.TransactionResult
 
 interface MempoolApi {
-    // TODO: revisit start_index path param
     @GET("block/{id}/txs")
-    suspend fun getBlockTransactions(
-        @Path("id") blockId: String,
-    ): List<TransactionResult>
+    suspend fun getBlockTransactions(@Path("id") blockId: String): List<TransactionResult>
 }
