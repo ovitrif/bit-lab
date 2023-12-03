ecd342e8d2398faaf3ab5412e6ed75416328c61d
Ovi Trif <octrif@gmail.com>
2023-12-03 19:24:00 +0100

    Add list of block-detail viewmodel tests

---
+++ app/src/test/java/tech/masivo/bitlab/ui/BlockDetailViewModelTest.kt
@@ -0,0 +1,36 @@
+package tech.masivo.bitlab.ui
+
+import org.junit.Before
+import org.junit.Test
+import org.mockito.kotlin.mock
+
+class BlockDetailViewModelTest {
+    private lateinit var viewModel: BlockDetailViewModel
+
+    @Before
+    fun setUp() {
+        viewModel = BlockDetailViewModel(
+            restApiClient = mock()
+        )
+    }
+
+    @Test
+    fun `fetchDetails should fetch transactions for given blockId`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `fetchDetails should emit ui state on api result`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `fetchDetails should map api transactions data to uiState`() {
+        TODO("Assert that api transaction data is correctly mapped to the ui state model")
+    }
+
+    @Test
+    fun `transaction uiState should switch expand value on toggle`() {
+        TODO("Not yet implemented")
+    }
+}
