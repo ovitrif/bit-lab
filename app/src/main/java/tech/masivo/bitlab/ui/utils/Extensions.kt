af0ca166647e39fafcd92bcb6cd28ab1c01ce722
Ovi Trif <octrif@gmail.com>
2023-12-03 22:44:58 +0100

    Refactor formatDate

--- app/src/main/java/tech/masivo/bitlab/ui/utils/Extensions.kt
+++ app/src/main/java/tech/masivo/bitlab/ui/utils/Extensions.kt
@@ -1,5 +1,6 @@
 package tech.masivo.bitlab.ui.utils
 
+import java.text.DecimalFormat
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
@@ -38,13 +39,8 @@ fun String.trimId(): String = this.takeLast(8)
 fun Long.satToBtc(): Double = this.toDouble() / 100_000_000
 
 /**
- * Formats a value representing a fee rate in sat/vB into a string representation.
- * If the value is an integer, it formats without decimal places.
- * Otherwise, it formats with 1 decimal place.
+ * Formats a value representing a fee rate in sat/vB into a string with maximum 1 decimal place.
  *
  * @return A string representation of the double value.
  */
-fun Double.formatRate(): String {
-    val decimals = if (this % 1.0 == 0.0) 0 else 1
-    return "%.${decimals}f".format(this)
-}
+fun Double.formatRate(): String = DecimalFormat("#.#").format(this)
