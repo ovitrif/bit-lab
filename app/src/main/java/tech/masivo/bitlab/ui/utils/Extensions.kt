a832eb83b37f9bf50ce40979f99c9f234d5b680b
Ovi Trif <octrif@gmail.com>
2023-12-03 21:39:00 +0100

    Add code docs to ui utils extensions

--- app/src/main/java/tech/masivo/bitlab/ui/utils/Extensions.kt
+++ app/src/main/java/tech/masivo/bitlab/ui/utils/Extensions.kt
@@ -6,25 +6,43 @@ import java.util.Locale
 
 private val BLOCK_DATE_FORMAT = SimpleDateFormat("HH:mm:ss", Locale.ROOT)
 
+/**
+ * @return A string representation of the unix timestamp in the "HH:mm:ss" format.
+ */
 fun Long.formatTimestamp(): String {
     return BLOCK_DATE_FORMAT.format(Date(this))
 }
 
+/**
+ * @return A string representation of the megabytes with 2 decimal places.
+ */
 fun Long.asMegabytes(): String {
-    val megabytes = this.toDouble() / 8 / 1024 / 102
-    return String.format("%.2f MB", megabytes)
+    return "%.2f MB".format(this.toDouble() / 8 / 1024 / 102)
 }
 
+/**
+ * @return A string containing the last 8 characters of the original.
+ */
 fun String.trimId(): String = this.takeLast(8)
 
 /**
  * Converts sat to btc (1 BTC = 100,000,000 sat)
  * TODO: use BigDecimal to avoid rounding errors
  */
+
+/**
+ * Converts a value representing satoshis into a Double representation of bitcoins.
+ *
+ * @return A double representation of the bitcoins.
+ */
 fun Long.satToBtc(): Double = this.toDouble() / 100_000_000
 
 /**
- * Formats to 1 decimal place if it has decimals, otherwise it will be an integer
+ * Formats a value representing a fee rate in sat/vB into a string representation.
+ * If the value is an integer, it formats without decimal places.
+ * Otherwise, it formats with 1 decimal place.
+ *
+ * @return A string representation of the double value.
  */
 fun Double.formatRate(): String {
     val decimals = if (this % 1.0 == 0.0) 0 else 1
