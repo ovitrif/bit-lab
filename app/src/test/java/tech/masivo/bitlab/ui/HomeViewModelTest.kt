113ea3bc171a4d588e150da7b232d8e5c176e172
Ovi Trif <octrif@gmail.com>
2023-12-03 18:57:53 +0100

    Add list of home viewmodel tests

---
+++ app/src/test/java/tech/masivo/bitlab/ui/HomeViewModelTest.kt
@@ -0,0 +1,31 @@
+package tech.masivo.bitlab.ui
+
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito.mock
+
+class HomeViewModelTest {
+    private lateinit var viewModel: HomeViewModel
+
+    @Before
+    fun setUp() {
+        viewModel = HomeViewModel(
+            webSocketClient = mock(),
+        )
+    }
+
+    @Test
+    fun `should fetch data on init`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `should emit data updates`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `should not emit empty blocks from updates`() {
+        TODO("Not yet implemented")
+    }
+}
