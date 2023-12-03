c473de89473349f830737479c4427798f6970a4c
Ovi Trif <octrif@gmail.com>
2023-12-03 21:28:49 +0100

    Add code docs to InfoRow

--- app/src/main/java/tech/masivo/bitlab/ui/components/InfoRow.kt
+++ app/src/main/java/tech/masivo/bitlab/ui/components/InfoRow.kt
@@ -2,7 +2,6 @@ package tech.masivo.bitlab.ui.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -13,26 +12,38 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 
+/**
+ * A Displays a row of information with a label and a value.
+ * If any of the parameters is null, the corresponding side of the row will be empty.
+ *
+ * @param label The label to be displayed on the left side of the row (optional).
+ * @param value The value to be displayed on the right side of the row (optional).
+*/
 @Composable
 fun InfoRow(
-    label: String = "",
-    value: String = "",
+    modifier: Modifier = Modifier,
+    label: String? = null,
+    value: String? = null,
 ) {
     Row(
-        modifier = Modifier
+        modifier = modifier
             .padding(vertical = 4.dp, horizontal = 16.dp),
         horizontalArrangement = Arrangement.spacedBy(4.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Text(
-            text = label,
-            style = MaterialTheme.typography.labelLarge.copy(fontWeight = FontWeight.Black)
-        )
-        Text(
-            text = value,
-            style = MaterialTheme.typography.labelLarge,
-            maxLines = 1,
-            overflow = TextOverflow.Ellipsis
-        )
+        label?.let {
+            Text(
+                text = label,
+                style = MaterialTheme.typography.labelLarge.copy(fontWeight = FontWeight.Black)
+            )
+        }
+        value?.let {
+            Text(
+                text = value,
+                style = MaterialTheme.typography.labelLarge,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
     }
 }
